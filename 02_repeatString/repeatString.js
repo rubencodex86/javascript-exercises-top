fbf4f352296b6030e711af8b4f2d776690f56c81
rubencodex86 <ruben.roia@outlook.pt>
2024-07-15 22:17:30 +0000

    Exercises 01 and 02 completed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = "";
+    if (num>=0) {
+        for (let i = 0; i < num; i++) {
+            finalString = finalString + string;
+        }
+        return finalString;
+    } else { return "ERROR" }
 };
 
 // Do not edit below this line
